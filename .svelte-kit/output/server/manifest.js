3f7896a88113cffc504291d87fa6298c0426c530
Rasmus Mikaelsson <ralle.mikaelsson@hotmail.com>
2023-07-29 21:51:00 +0200

    build #2

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["banner-main.JPG","banner-sandning.JPG","butik-main.JPG","favicon.png","IMG_E4615.JPG","IMG_E4619.JPG","IMG_E4620.JPG","sandning-main.JPG","snorojning-main.JPG","sopning-main.png","specialtransport.JPG","test.JPG","transport.JPG"]),
 	mimeTypes: {".JPG":"image/jpeg",".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.bd4092c6.js","app":"_app/immutable/entry/app.b30da7fa.js","imports":["_app/immutable/entry/start.bd4092c6.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.e94e6294.js","_app/immutable/entry/app.b30da7fa.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d39bed27.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.fc77312b.js","app":"_app/immutable/entry/app.0e7af499.js","imports":["_app/immutable/entry/start.fc77312b.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.d50dde57.js","_app/immutable/entry/app.0e7af499.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d39bed27.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.1fd0ee96.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d39bed27.js","_app/immutable/chunks/singletons.e94e6294.js"];
+export const imports = ["_app/immutable/nodes/1.3f08e789.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d39bed27.js","_app/immutable/chunks/singletons.d50dde57.js"];
 export const stylesheets = [];
 export const fonts = [];
